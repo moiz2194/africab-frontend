6ce15ac41872a6835637946b46a6962f9524c041
moiz2194 <94681685+moiz2194@users.noreply.github.com>
2023-12-04 03:47:48 +0500

    Update State.js

--- context/State.js
+++ context/State.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import context from './context.js';
 import { useSearchParams } from 'react-router-dom';
-const host = "https://africabackend.onrender.com"
+const host = process.env.BACKEND
 
 const State = (props) => {
   const [user, setuser] = React.useState()
